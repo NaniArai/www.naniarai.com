b1e8e6ca64d1a35c891abd5bbeadcb08e0db9c18
SaKKo <saklism@gmail.com>
2021-03-25 18:51:43 +0700

    1ff178c

---
+++ _nuxt/f3ee48d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{314:function(n,t,e){"use strict";e.r(t);var c={},r=e(47),component=Object(r.a)(c,(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",{staticClass:"container"},[n._v("Page under construction")])}),[],!1,null,null,null);t.default=component.exports}}]);
